a81a522b5307befcca40d4b657a51d509b6ae9e9
liviarez <mrezende.livia@gmail.com>
2023-06-20 11:01:38 -0300

    Feat: Req 1 - Implements Login Component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isLoginButtonDisabled: false,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.verifyLoginComponent());
+  };
+
+  verifyLoginComponent = () => {
+    const { email, password } = this.state;
+    const minimiumCharacters = 6;
+    const verifyPassword = password.length >= minimiumCharacters;
+    const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const verifyEmail = emailRegex.test(email);
+    this.setState({
+      isLoginButtonDisabled: verifyPassword && verifyEmail,
+    });
+  };
+
+  handleClick = () => {
+    const { email } = this.state;
+    // função para enviar para o estado global, espera receber uma action
+    const { dispatch, history } = this.props;
+    dispatch(addEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isLoginButtonDisabled } = this.state;
+    return (
+      <form>
+        <input
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+          placeholder="E-mail"
+          data-testid="email-input"
+        />
+        <input
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+          placeholder="Senha"
+          data-testid="password-input"
+        />
+        <button
+          type="button"
+          onClick={ this.handleClick }
+          disabled={ !isLoginButtonDisabled }
+        >
+          Entrar
+
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape(
+    { push: PropTypes.func.isRequired },
+  ).isRequired,
+
+};
+// quando for usar o estado global eu preciso usar esse connect
+export default connect()(Login);
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { ADD_EMAIL } from '../actions';
+
+const INITIAL_STATE = { email: '' };
+
+const user = (state = INITIAL_STATE, action) => {
+  console.log(action.type);
+  switch (action.type) {
+  case ADD_EMAIL:
+    return {
+      email: action.payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
